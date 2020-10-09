859d1a3deb182408184e94e1f5518803fc2c9a9c
rubengr16 <ruben16gallego@gmail.com>
2020-10-09 18:25:08 +0200

    RightTrafficLight setter&getter

--- src/RightTrafficLight.java
+++ src/RightTrafficLight.java
@@ -1,8 +1,23 @@
 public class RightTrafficLight extends TrafficLight{ // extends TrafficLight methods
-    private int RightColour;
+    private int rightColour;
 
     public RightTrafficLight() {
-        this.colour = RED;
-        this.RightColour = RED;
+        super(TrafficLight.RED);
+        setRightColour(TrafficLight.RED);
     }
+
+    public RightTrafficLight( int colour, int rightColour) {
+        super(colour);
+        setRightColour(rightColour);
+    }
+
+    public void setRightColour(int rightColour) {
+        this.rightColour = rightColour;
+    }
+
+    public int getRightColour(){
+        return rightColour;
+    }
+
+
 }
