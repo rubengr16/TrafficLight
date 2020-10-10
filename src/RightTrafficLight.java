b1f2fab6ca933f99a312a56b734346b1dd024949
rubengr16 <ruben16gallego@gmail.com>
2020-10-10 15:56:53 +0200

    TrafficLight completed

--- src/RightTrafficLight.java
+++ src/RightTrafficLight.java
@@ -1,23 +1,26 @@
 public class RightTrafficLight extends TrafficLight{ // extends TrafficLight methods
     private int rightColour;
 
-    public RightTrafficLight() {
+    public RightTrafficLight () {
         super(TrafficLight.RED);
         setRightColour(TrafficLight.RED);
     }
 
-    public RightTrafficLight( int colour, int rightColour) {
+    public RightTrafficLight ( int colour, int rightColour) {
         super(colour);
         setRightColour(rightColour);
     }
 
-    public void setRightColour(int rightColour) {
+    public void setRightColour (int rightColour) {
         this.rightColour = rightColour;
     }
 
-    public int getRightColour(){
+    public int getRightColour (){
         return rightColour;
     }
 
+    public String toString () { // Overriding
+        return super.toString() + "," + rightColour;
+    }
 
 }
